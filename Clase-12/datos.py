73b24b3996bd38c6b3853712acbe8ef6a91280b8
demoster18 <134543104+demoster18@users.noreply.github.com>
2023-05-24 14:34:04 -0400

    Archivo datos.py sin Validar

---
+++ Clase-12/datos.py
@@ -0,0 +1,11 @@
+
+
+print("DATOS PERSONALES")
+print("---------------- \n" )
+vnom = input("Ingrese su Nombre: ") 
+vEdad = input("Ingrese su Edad: ")
+print("---------------------------")
+print(f"Su Nombre es: {vnom}")
+print(f"Su Edad es: {vEdad}")
+
+print("Programa Finalizado...")
